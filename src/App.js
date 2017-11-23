5d637f02bda591228d3b575ac259fd942e4efbac
Sandip Pal <sandip@asteria.co.in>
2017-11-23 15:04:30 +0530

    Adds Transition
     -  installs react addone
     -  modifies app file
     -  adds sass file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
 import Jumbotron from './components/common/Jumbotron';
 import Button from './components/common/Button';
+import './App.scss';
+
+// import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+// var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+// const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+import { CSSTransitionGroup } from 'react-transition-group';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Jumbotron className="banner-jumbotron" containerfluid="true">
-            <header className="App-header">
-              <h1 className="App-title">Welcome to React</h1>
-            </header>
-            <p className="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-            <Button className="btn-primary btn-lg" disabled href="https://google.co.in">Hello, Where??</Button>
-            <Button className="btn-danger" disabled>Hello i am </Button>
-            <Button className="btn-info btn-block" href="https://google.co.in">This Button Block</Button>
-        </Jumbotron>
+        <CSSTransitionGroup 
+          transitionName="appfade"
+          transitionAppear={true}
+          transitionAppearTimeout={50000}
+          transitionLeaveTimeout={3000}
+          transitionEnter={false}
+          transitionLeave={false}>
+            <Jumbotron className="banner-jumbotron" containerfluid="true">
+                <header className="App-header">
+                  <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                  To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <Button className="btn-primary btn-lg" disabled href="https://google.co.in">Hello, Where??</Button>
+                <Button className="btn-danger" disabled>Hello i am </Button>
+                <Button className="btn-info btn-block" href="https://google.co.in">This Button Block</Button>
+            </Jumbotron>
+        </CSSTransitionGroup>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: ['babel-polyfill', './src/index.js'],
@@ -15,7 +16,34 @@ module.exports = {
         loaders: [
             {test: /\.js$/,
             exclude: /node_modules/,
-            loader: 'babel-loader'}
+            loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader'
+            },
+            {test: /\.css$/,
+            use: ['css-loader'],
+            // use: ExtractTextPlugin.extract(['css'])
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader", options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: "sass-loader", options: {
+                        sourceMap: true
+                    }
+                }]
+            }
         ]
-    }
+    },
+    devtool: "source-map",
+    plugins: [
+        new ExtractTextPlugin('app.css')
+    ]
 }
